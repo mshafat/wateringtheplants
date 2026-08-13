0fa99e08dd433f41a2e014b202423d88ea5c94e0
Shafat M. (DigiShikhon) <65461557+mshafat@users.noreply.github.com>
2026-08-13 16:23:39 +0600

    Update astro-paper.config.ts

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -3,13 +3,13 @@ import { defineAstroPaperConfig } from "./src/types/config";
 export default defineAstroPaperConfig({
   site: {
     url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
-    description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
+    title: "Watering the plants",
+    description: "From watering the plants to finding a sidehuslte",
+    author: "Shafat M.",
+    profile: "https://www.threads.com/@digishikhon",
     ogImage: "default-og.jpg",
     lang: "en",
-    timezone: "Asia/Bangkok",
+    timezone: "Asia/Dhaka",
     dir: "ltr",
   },
   posts: {
@@ -29,10 +29,9 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    { name: "github",   url: "https://github.com/mshafat" },
+    { name: "threads",        url: "https://www.threads.com/@digishikhon" },
+    { name: "mail",     url: "mailto:m.shafat@yahoo.co.uk" },
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
